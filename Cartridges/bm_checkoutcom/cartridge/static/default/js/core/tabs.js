d6f89d4dda2c9221d20c46c0e4c52c41ab41dc86
david-fiaty-cko <david.fiaty@checkout.com>
2020-07-25 02:08:25 +0100

    Excluded a function from the lint scope

--- Cartridges/bm_checkoutcom/cartridge/static/default/js/core/tabs.js
+++ Cartridges/bm_checkoutcom/cartridge/static/default/js/core/tabs.js
@@ -4,7 +4,7 @@
  * Set the navigation state.
  * @param {string} path The navigation path
  */
-function setNavigationstate(path) {
+function setNavigationstate(path) { // eslint-disable-line
     // Get the path members
     var members = path.split('/');
 
